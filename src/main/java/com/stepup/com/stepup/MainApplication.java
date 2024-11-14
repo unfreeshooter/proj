a8fdcd6255a6451bcecac3ecba7f235510cfff07
Alex Lazarev <unfreeshooter@gmail.com>
2024-11-14 22:47:02 +0300

    Изменена основная логика Добавлен класс коробки

--- src/main/java/com/stepup/com/stepup/MainApplication.java
+++ src/main/java/com/stepup/com/stepup/MainApplication.java
@@ -3,6 +3,9 @@ package com.stepup.com.stepup;
 public class MainApplication {
     public static void main(String[] args) {
         System.out.println("Hello World");
-        System.out.println("1111111");
+        System.out.println("1100011");
+
+        System.out.println("2222222");
+        System.out.println("33333333");
     }
 }
